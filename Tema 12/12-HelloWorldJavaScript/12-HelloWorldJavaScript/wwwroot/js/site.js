778c3a3fed57cd908b68e02d6d52f8582a55a6f8
erkakii <93649044+erkakii@users.noreply.github.com>
2023-01-12 14:38:51 +0100

    empezamos con javascript

---
+++ Tema 12/12-HelloWorldJavaScript/12-HelloWorldJavaScript/wwwroot/js/site.js
@@ -0,0 +1,27 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+window.onload = inicializarEventos;
+
+
+function inicializarEventos() {
+    var miBoton = document.getElementById("btnSaludar");
+    var imagen = document.getElementById("imgGalleta");
+
+    imagen.addEventListener("click", sumarContador, false)
+    miBoton.addEventListener("click", saludar, false);
+
+}
+
+
+function saludar() {
+    document.getElementById("btnSaludar").value = "Despedir";
+}
+
+function sumarContador() {
+    var contador = document.getElementById("lblPulsaciones");
+    contador.textContent++;
+
+}
