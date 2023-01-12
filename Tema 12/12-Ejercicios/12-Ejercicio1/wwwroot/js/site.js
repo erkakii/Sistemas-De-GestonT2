06f8923ecc539d2398561cda72cba0840ff58a96
Álvaro Castro Sánchez <alvaro.castro@iesnervion.es>
2023-01-13 00:34:07 +0100

    haciendo los ejercicios

---
+++ Tema 12/12-Ejercicios/12-Ejercicio1/wwwroot/js/site.js
@@ -0,0 +1,30 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+window.onload = inicializarEventos;
+
+class Persona {
+    constructor(nombre, apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+}
+
+
+function inicializarEventos() {
+    var miBoton = document.getElementById("btnSaludar");
+    miBoton.addEventListener("click", saludar, false)
+    
+
+}
+
+function saludar() {
+    var nombre = document.getElementById("Nombre").value
+    var apellido = document.getElementById("Apellido").value
+    var persona = new Persona(nombre, apellido);
+    var saludo = "Hola: " + persona.nombre + " " + persona.apellido
+    alert(saludo)
+}
---
+++ Tema 12/12-Ejercicios/12-Ejercicio2/wwwroot/js/site.js
@@ -0,0 +1,39 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+window.onload = inicializarEventos
+
+function inicializarEventos() {
+    var opt_1 = new Array("-", "Focus", "Puma", "Mondeo", "Kuga", "Fiesta");
+    var opt_2 = new Array("-", "Twingo", "Space", "Captur", "Scenic");
+    var opt_3 = new Array("-", "Ibiza", "Leon");
+    var formulario = document.getElementById("formulario1")
+    formulario.addEventListener("onchange", cambio, false)
+
+}
+
+function cambio() {
+    var cosa;
+
+    cosa = document.getElementById("formulario1").marcas[document.getElementById("formulario1").marcas.selectedIndex.value];
+
+    if (cosa != 0) {
+        mis_opts = eval("opt_" + cosa);
+        num_opts = mis_opts.lenght;
+        document.getElementById("formulario1").getElementById("modelos").lenght = num_opts;
+        for (i = 0; i < num_opts; i++) {
+            document.getElementById("formulario1").getElementById("modelos").options[i].value = mis_opts[i];
+            document.getElementById("formulario1").getElementById("modelos").options[i].text = mis_opts[i];
+        }
+    } else {
+        document.getElementById("formulario1").getElementById("modelos").lenght = 1;
+        //ponemos un guion en la unica opcion que he dejado
+        document.getElementById("formulario1").getElementById("modelos").options[0].value = "-";
+        document.getElementById("formulario1").getElementById("modelos").options[0].text = "-";
+    }
+
+    //hacer un reset de las opciones
+    document.getElementById("formulario1").getElementById("modelos").options[0].selected = true;
+}
