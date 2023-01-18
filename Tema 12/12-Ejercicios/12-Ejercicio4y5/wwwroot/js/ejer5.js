d7ed8933503169fba2d7cde0b9ee3ec3251adc12
erkakii <93649044+erkakii@users.noreply.github.com>
2023-01-18 09:54:09 +0100

    fantasticmaceta

--- Tema 12/12-Ejercicios/12-Ejercicio4y5/wwwroot/js/ejer5.js
+++ Tema 12/12-Ejercicios/12-Ejercicio4y5/wwwroot/js/ejer5.js
@@ -16,7 +16,8 @@ function inicio(){
     var imgs1 = document.getElementById("digito1Segundos")
     var imgs2 = document.getElementById("digito2Segundos")
     var today = new Date();
-    var now = today.toLocaleTimeString('es-ES');
+    var now = today.toLocaleTimeString('es-ES', { hour: "2-digit", minute: "2-digit", second: "2-digit" });
+    
 
     var valores = now.split(":");
     var h1 = valores[0].charAt(0);
@@ -25,6 +26,8 @@ function inicio(){
     var m2 = valores[1].charAt(1);
     var s1 = valores[2].charAt(0);
     var s2 = valores[2].charAt(1);
+
+    
 
     imgh1.src = "../img/" + h1 + ".gif"
     imgh2.src = "../img/" + h2 + ".gif"
